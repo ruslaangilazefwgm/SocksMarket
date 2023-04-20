7e17f5f4cf8d453cd3a08e348439e235103856df
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2023-04-20 20:46:30 +0300

    initial

---
+++ src/main/java/com/socksmarker/socksmarket/SocksMarketApplication.java
@@ -0,0 +1,13 @@
+package com.socksmarker.socksmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socksmarker/socksmarket/SocksMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socksmarker.socksmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
